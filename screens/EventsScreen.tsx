6af795a0af539a2ea43083e6072fa774a735e89e
coletcarlson <colecarls13@gmail.com>
2023-01-09 01:02:10 -0600

    Update to create list from mongoDB server

--- screens/EventsScreen.tsx
+++ screens/EventsScreen.tsx
@@ -4,24 +4,35 @@ import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { ScrollView } from 'react-native';
 import { RootTabScreenProps } from '../types';
+import { useEffect, useState } from 'react';
 
 export default function EventsScreen() {
+  const [data, setData] = useState<any[]>([]);
+  const [loading, setLoading] =  useState(true);
 
-  const data = [
-    {key: 'Test1'},
-    {key: 'Test2'}
-  ]
+  const getEvents = async () => {
+    const res = await fetch('http://localhost:8000/events');
+    const data = await res.json();
+    setData(data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getEvents();
+  }, []);
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Events Screen</Text>
       <FlatList
         data={data}
-        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        renderItem={({item}) => <Text style={styles.item}>{item.eventname}</Text>}
       />
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
